92a4a61e02a3f3ec99bf3ebad738040b15bafc88
christso <christso@gmail.com>
2015-12-26 22:41:55 +1100

    add Xafology.UnitTests, renamed Xafology.Tests to Xafology.TestUtils

--- Xafology.Tests/InMemoryTestBase.cs
+++ Xafology.Tests/InMemoryTestBase.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Xafology.Tests
+namespace Xafology.TestUtils
 {
     public class InMemoryDbTestBase
     {
--- Xafology.UnitTests/Properties/AssemblyInfo.cs
+++ Xafology.UnitTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PivotGridLayout.Web")]
+[assembly: AssemblyTitle("Xafology.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("-")]
-[assembly: AssemblyProduct("PivotGridLayout.Web")]
-[assembly: AssemblyCopyright("Copyright © - 2007")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xafology.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,6 +19,9 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3923d865-b87c-49e9-8f09-836f45fa47f5")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -26,6 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.3")]
