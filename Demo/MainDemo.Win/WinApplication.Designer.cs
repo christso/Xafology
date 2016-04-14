f0d115bb00fc31c5a02e7b3d7de64114b2dc599e
Christopher Tso <christso@gmail.com>
2016-04-14 20:55:17 +1000

    added MainDemo which includes SequentialBaseObject

---
+++ Demo/MainDemo.Win/WinApplication.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace MainDemo.Win {
+    partial class MainDemoWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module4 = new MainDemo.Module.Win.MainDemoWindowsFormsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // MainDemoWindowsFormsApplication
+            // 
+            this.ApplicationName = "PasteDemo";
+            this.LinkNewObjectToParentImmediately = false;
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(sequentialBaseModule = new Xafology.ExpressApp.Xpo.SequentialBase.XafologySequentialBaseModule());
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.MainDemoWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.MainDemoWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private MainDemo.Module.Win.MainDemoWindowsFormsModule module4;
+        private Xafology.ExpressApp.Xpo.SequentialBase.XafologySequentialBaseModule sequentialBaseModule;
+    }
+}
