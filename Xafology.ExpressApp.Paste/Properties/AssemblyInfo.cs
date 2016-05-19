64631bdd68f577f512e513ff7347ca04d944a18d
Christopher Tso <christso@gmail.com>
2016-05-20 08:09:34 +1000

    add Paste module

---
+++ Xafology.ExpressApp.Paste/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xafology.ExpressApp.Paste")]
+[assembly: AssemblyDescription("Xafology")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DG2NTT Pty Ltd")]
+[assembly: AssemblyProduct("Xafology.ExpressApp.Paste.Win")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("DG2NTT")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3b6e1fa-5335-4805-954c-e7de8597df06")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Xafology.Data.AssemblyInfo.Version)]
