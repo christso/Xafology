1d096e23c4829e51626afba9286a754ddf43a4b2
Christopher Tso <christso@gmail.com>
2016-10-06 22:18:39 +1100

    increase database file size

--- Xafology.Utils/Data/MSSqlClientHelper.cs
+++ Xafology.Utils/Data/MSSqlClientHelper.cs
@@ -47,11 +47,11 @@ END";
                     command.CommandText = "CREATE DATABASE " + database + " ON PRIMARY " +
         "(NAME = " + database + "_Data, " +
         "FILENAME = '" + Path.Combine(dbDir, database + ".mdf'") + ", " +
-        "SIZE = 5MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+        "SIZE = 5MB, MAXSIZE = 9999MB, FILEGROWTH = 10%) " +
         "LOG ON (NAME = " + database + "_Log, " +
         "FILENAME = '" + Path.Combine(dbDir, database) + ".ldf', " +
         "SIZE = 1MB, " +
-        "MAXSIZE = 5MB, " +
+        "MAXSIZE = 9999MB, " +
         "FILEGROWTH = 10%)";
                     
                     command.ExecuteNonQuery();
